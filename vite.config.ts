adc5575cadc3441da185719b4799ac754a0453e3
Ivan Navarro <ivaneffable@gmail.com>
2026-01-22 16:29:47 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyReactRouter(),
+    netlify(),
+  ],
 });
