6233ce51bed084d2e23ef76b7c2f983bce41aebc
Ivan Navarro <ivaneffable@gmail.com>
2026-01-27 13:19:47 +0100

    adding missing meta

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#D97706" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <Meta />
         <Links />
       </head>
